afeb868c89695a60a1bbfb6b4779101e1c066cd1
Wong Oi In <christywong0218@gmail.com>
2019-05-02 10:45:02 +0800

    Update latest sql

--- application/modules/admin/controllers/Job.php
+++ application/modules/admin/controllers/Job.php
@@ -4,16 +4,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Job extends Admin_Controller {
 
 	public function index()
-	{
+	{   
+        // Generate cruds for the table 'Jobs'
         $crud = $this->generate_crud('jobs');
         
+        // Setting for table relation 1-1
+        // Method: set_relation($field_name , $related_table, $related_title_field)
 		$crud->set_relation('district_id', 'districts', 'name_en');
-		$crud->set_relation('employer_user_id', 'employer_users', 'name_en');
-		// disable direct create / delete Frontend User
-        // $crud->unset_add();
-        // $crud->unset_edit();
-        // $crud->unset_delete();
+        $crud->set_relation('employer_user_id', 'employer_users', 'name_en');
         
+        // Render crud view
         $this->mPageTitle = 'All Jobs Posted';
         $this->render_crud();
 	}
